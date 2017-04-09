24c7427eaef4cc2f26de6d830e3f365de470e5b0
Yannick Lanz <yannick.lanz@gmail.com>
2017-04-09 14:09:33 +0200

    Modified to support the LORIX One

---
+++ libloragw/inc/lorixone.h
@@ -0,0 +1,19 @@
+/*
+ *  lorixone.h
+ *
+ *  Created on: April 9, 2017
+ *      Author: lanzy
+ */
+
+#ifndef _LORIXONE_H_
+#define _LORIXONE_H_
+
+/* Human readable platform definition */
+#define DISPLAY_PLATFORM "LORIX One"
+
+/* parameters for native spi */
+#define SPI_SPEED       8000000
+#define SPI_DEV_PATH    "/dev/spidev32766.0"
+#define SPI_CS_CHANGE   0
+
+#endif /* _LORIXONE_H_ */
